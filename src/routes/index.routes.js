b1a888ea31d33865aa104ffe88e5e4598aca9001
Matias <matias.b.dev@gmail.com>
2023-03-08 01:46:36 -0300

    agrege varias funciones al programa .

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import Bomberos from "../models/Bomberos";
+import Unidades from "../models/Unidades";
+import Novedades from "../models/Novedades";
 
 const router = Router();
 
@@ -9,15 +11,66 @@ router.get("/", (req, res) => {
 
 //Rutas de Modulo Bomberos
 
-router.get("/Bomberos", (req, res) => {
-  res.render("bomberos");
+router.get("/Bomberos", async (req, res) => {
+  const TodosBomberos = await Bomberos.find().lean();
+  res.render("Bomberos", { TodosBomberos: TodosBomberos });
 });
 
-router.post("/bomberos/agregar", async(req, res) => {
-  const bomberos = Bomberos(req.body);
-  const bomberoguardado =  await bomberos.save();
-
-  console.log(bomberoguardado);
-  res.send("bombero agregado al abase de datos ");
+router.post("/bomberos/agregar", async (req, res) => {
+  const bombero = Bomberos(req.body);
+  await bombero.save();
+  res.redirect("/bomberos");
 });
+
+router.get("/bomberoAdd", (req, res) => {
+  res.render("bomberoAdd");
+});
+
+//Rutas Unidades
+
+router.get("/unidades", async (req, res) => {
+  const unidad = await Unidades.find().lean();
+
+  res.render("unidades", { unidad: unidad });
+});
+
+router.get("/unidadesAdd", (req, res) => {
+  res.render("unidadesAdd");
+});
+
+router.post("/unidades/agregar", async (req, res) => {
+  const unidades = Unidades(req.body);
+  await unidades.save();
+  res.redirect("/unidades");
+});
+
+// rutas novedades.
+
+router.get("/novedades", async (req, res) => {
+  const novedad = await Novedades.find().lean().sort({_id:-1});
+  novedad.sort();
+
+  res.render("novedades", { novedad: novedad });
+});
+
+router.get("/novedadesAdd", async(req, res) => {
+  const bombero = await Bomberos.find().lean();
+  res.render("novedadesAdd", {bombero: bombero});
+});
+
+router.post("/novedades/agregar", async (req, res) => {
+  const novedades = Novedades(req.body);
+  await novedades.save();
+  res.redirect("/novedades");
+});
+
+//movimientos Unidades
+
+router.get("/movimientos",  (req, res) => {
+  
+
+  res.render("movimientoUnidades");
+});
+
+
 export default router;
