0495c13284d19ad0011528ed664d70bb09a9331e
Matias <matias.b.dev@gmail.com>
2023-05-04 01:34:09 -0300

    BASE DATOS

--- src/database.js
+++ src/database.js
@@ -4,7 +4,7 @@ import mg from "mongoose";
 (async () => {
   try {
     mg.set("strictQuery", false);
-    const db = await connect("mongodb://127.0.0.1:27017/BDbvsf");
+    const db = await connect("mongodb+srv://matiasbdev:rxjeQQvoPWSTigeZ@bvsf.9vfczge.mongodb.net/?retryWrites=true&w=majority");
     console.log("BD conectada con", db.connection.name);
   } catch (error) {
     console.error(error);
