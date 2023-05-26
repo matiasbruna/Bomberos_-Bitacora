a3e0fd25766fd2ed1daff7abf48d310b06b26031
Matias <matias.b.dev@gmail.com>
2023-05-26 20:41:08 -0300

    agrege carpeta de vista de unidades y corregi codigo del archivo de rutas para su uso

--- src/routes/unidades.routes.js
+++ src/routes/unidades.routes.js
@@ -7,17 +7,17 @@ router.get("/unidades", async (req, res) => {
   const unidad = await Unidades.find().lean();
   console.log(unidad);
 
-  res.render("unidades", { unidad: unidad });
+  res.render("unidades/unidades", { unidad: unidad });
 });
 
 router.get("/unidadesAdd", (req, res) => {
-  res.render("unidadesAdd");
+  res.render("unidades/unidadesAdd");
 });
 
 router.post("/unidades/agregar", async (req, res) => {
   const unidades = Unidades(req.body);
   await unidades.save();
-  res.redirect("/unidades");
+  res.redirect("unidades/unidades");
 });
 
 export default router;
