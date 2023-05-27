3bf73683203b814aa0612cbfe273755be13d1aae
Matias <matias.b.dev@gmail.com>
2023-05-26 23:44:53 -0300

    se trabajo en la funcionalidad de novedades agregando carpetas y organizando codigo

--- src/routes/novedades.routes.js
+++ src/routes/novedades.routes.js
@@ -7,16 +7,17 @@ router.get("/novedades", async (req, res) => {
   const novedad = await Novedades.find().lean().sort({ _id: -1 });
   novedad.sort();
 
-  res.render("novedades", { novedad: novedad });
+  res.render("novedades/novedades", { novedad: novedad });
 });
 
 router.get("/novedadesAdd", async (req, res) => {
   const bombero = await Bomberos.find().lean();
-  res.render("novedadesAdd", { bombero: bombero });
+  res.render("novedades/novedadesAdd", { bombero: bombero });
 });
 
-router.post("/novedades/agregar", async (req, res) => {
+router.post("/novedadesAgregar", async (req, res) => {
   const novedades = Novedades(req.body);
+  console.log(novedades);
   await novedades.save();
   res.redirect("/novedades");
 });
