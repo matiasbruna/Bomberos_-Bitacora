190c4bd076697681a1c2e4ac3e8a365571897743
Matias <matias.b.dev@gmail.com>
2023-08-22 16:51:54 -0300

    se modifico variable de entorno para poder hacer el logeo mas seguro y se corrigio Error de ortografia en la vista acercaDE

--- src/models/auth.js
+++ src/models/auth.js
@@ -1,3 +1,3 @@
-export let User = ["BRUNA, Matias Guillermo"]; //lo dejo cargado para poder usar todas las funciones 
+export let User = []; // cuando estes desarrolando lo dejo cargado para poder usar todas las funciones , 
 //sino tengo que logearme a cada rato.
-export let Admin = ["BRUNA, Matias Guillermo"];
+export let Admin = [];
